5af46e5a4d50c0fc974e09585b3da13cc77794ff
p22mendizabal <p22lmendizabal@fairfieldprep.org>
2021-10-07 23:28:07 -0400

    hw-3-2-1 Complete

--- hw-3-2-1.py
+++ hw-3-2-1.py
@@ -1,15 +1,35 @@
 # author: LM 10/06/21
 average_grade = int(input("Enter in the numerical average you had this quarter. "))
 
-if average_grade >= 93:
-    print("You have an A")
+if average_grade < 60:
+    print("You got an F")
 else:
-    if average_grade < 93:
-        print("You have an A-")
-    else: 
-        print("You have an A.")
-if average_grade <= 89:
-    print("You have a B+")
-else:
-    if average_grade <= 86.999999999:
-        print("You have a B.")
+    if average_grade < 65:
+        print("You have a D")
+    else:
+        if average_grade < 70:
+            print("You have a D+")
+        else:
+            if average_grade < 73:
+                print("You have a C-")
+            else:
+                if average_grade < 77:
+                    print("You have a C")
+                else:
+                    if average_grade < 80:
+                        print("You have a C+")
+                    else:
+                        if average_grade < 83:
+                            print("you have a B-")
+                        else:
+                            if average_grade < 87:
+                                print("You have a B")
+                            else:
+                                if average_grade < 90:
+                                    print("You have a B+")
+                                else:
+                                    if average_grade < 93:
+                                        print("You have a A-")
+                                    else:
+                                        if average_grade < 100:
+                                            print("You have a A!!!!!")
